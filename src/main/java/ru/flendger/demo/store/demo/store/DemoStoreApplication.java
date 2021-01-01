a3a879efc6eaf215c04c0de7e639c2e1f2229d68
flendger <kiru.ee@yandex.ru>
2021-01-01 13:24:52 +0300

    initial commit

---
+++ src/main/java/ru/flendger/demo/store/demo/store/DemoStoreApplication.java
@@ -0,0 +1,13 @@
+package ru.flendger.demo.store.demo.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/flendger/demo/store/demo/store/DemoStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.flendger.demo.store.demo.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
